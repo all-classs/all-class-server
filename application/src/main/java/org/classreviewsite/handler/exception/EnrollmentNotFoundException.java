84a125f7dbdacf3a1368abcb6e181c705b5d2a89
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2025-08-11 03:16:18 +0900

    feat: 수강한 강의가 아닌 예외

---
+++ application/src/main/java/org/classreviewsite/handler/exception/EnrollmentNotFoundException.java
@@ -0,0 +1,7 @@
+package org.classreviewsite.handler.exception;
+
+public class EnrollmentNotFoundException extends RuntimeException {
+    public EnrollmentNotFoundException(String message) {
+        super(message);
+    }
+}
