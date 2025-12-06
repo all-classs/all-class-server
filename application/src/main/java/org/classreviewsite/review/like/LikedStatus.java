384184b55365a9e9589e3d1054df18442720f5ce
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2025-12-06 18:29:09 +0900

    refactor: 좋아요 상태 분리

---
+++ application/src/main/java/org/classreviewsite/review/like/LikedStatus.java
@@ -0,0 +1,19 @@
+package org.classreviewsite.review.like;
+
+public enum LikedStatus {
+    ALREADY_LIKE("이미 좋아요한 상태"),
+    POSSIBLE_LIKE("좋아요 가능한 상태"),
+
+    SAVE_LIKE_HISTORY("좋아요 기록을 저장하였음"),
+    DELETE_LIKE_HISTORY("좋아요 기록을 삭제하였음");
+
+    private final String message;
+
+    LikedStatus(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
