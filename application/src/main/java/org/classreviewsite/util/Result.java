e9f894177073df7970dd78e433bbe5176c751622
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2025-12-08 21:52:49 +0900

    refactor: renamed from ApiResponse.java

--- application/src/main/java/org/classreviewsite/util/Result.java
+++ application/src/main/java/org/classreviewsite/util/Result.java
@@ -5,28 +5,28 @@ import lombok.ToString;
 
 @Getter
 @ToString
-public class ApiResponse<T>{
+public class Result<T>{
 
     private final ResultCode code;
     private final T data;
     private final String message;
 
-    private ApiResponse(ResultCode code, T data, String message) {
+    private Result(ResultCode code, T data, String message) {
         this.code = code;
         this.data = data;
         this.message = message;
     }
 
-    public static <T> ApiResponse<T> success(T data, String message) {
-        return new ApiResponse<>(
+    public static <T> Result<T> success(T data, String message) {
+        return new Result<>(
                 ResultCode.SUCCESS,
                 data,
                 message
         );
     }
 
-    public static <T> ApiResponse<T> failure(ResultCode resultCode, String message) {
-        return new ApiResponse<>(
+    public static <T> Result<T> failure(ResultCode resultCode, String message) {
+        return new Result<>(
                 resultCode,
                 null,
                 message
