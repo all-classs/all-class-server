bafeadbb34c89c67ddcdeea34df925064e99b105
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2025-12-08 21:53:16 +0900

    refactor: renamed from ApiResponse -> Result , Swagger annotation fix

--- application/src/main/java/org/classreviewsite/review/controller/ReviewController.java
+++ application/src/main/java/org/classreviewsite/review/controller/ReviewController.java
@@ -2,10 +2,11 @@ package org.classreviewsite.review.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.classreviewsite.util.ApiResponse;
+import org.classreviewsite.util.Result;
 import org.classreviewsite.review.controller.data.Response.ReviewMeResponse;
 import org.classreviewsite.review.controller.data.Request.DeleteReviewRequest;
 import org.classreviewsite.review.controller.data.Request.LikeRequest;
@@ -23,82 +24,82 @@ public class ReviewController {
 
     private final ReviewService reviewService;
 
-    @GetMapping("/review")
-    @Operation(summary = "수강 후기 전체 목록 조회", description = "Param 설명 :lectureId 꼭 입력해주셔야됩니다. lowness true일 경우 별점 낮은 순 조회입니다. likes true일 경우 좋아요 높은순 조회입니다. recent true일 경우 날짜 최신순 조회입니다. lectureId만 줄 경우 기본값 별점 높은순 조회입니다. ex) ?lectureId=1&lowness=true , ?lectureId=1")
-    @ApiResponse(responseCode = "200", description = "수강 후기 전체 목록 조회입니다.")
-    @ApiResponse(responseCode = "202", description = "수강 후기가 어디에도 없습니다.")
-    public ApiResponses ReviewPostList(@RequestParam("lectureId") Long lectureId,
-                                       @RequestParam(value = "lowness" , required = false) boolean lowness,
-                                       @RequestParam(value = "likes", required = false) boolean likes,
-                                       @RequestParam(value = "recent" , required = false) boolean recent
-    ){
-        if(lowness == true){
-            log.info("별점 낮은 순");
-            List<ReviewResponse> response = reviewService.findByLectureIdOrderByStarLatingAsc(lectureId);
-            return ApiResponses.success( response, "수강 후기 별점 낮은 순 조회입니다.");
-        }else if(likes == true){
-            log.info("좋아요 높은 순");
-            List<ReviewResponse> response = reviewService.findByLectureIdOrderByLikesDesc(lectureId);
-            return ApiResponses.success(response, "수강 후기 좋아요 높은 순 조회입니다.");
-        }else if(recent == true){
-            log.info("날짜 최신순");
-            List<ReviewResponse> response = reviewService.findByLectureIdOrderByCreateDateDesc(lectureId);
-            return ApiResponses.success(response, "수강 후기 날짜 최신순 조회입니다.");
-        }
-
-        log.info("별점 높은 순");
-        List<ReviewResponse> response = reviewService.findByLectureIdOrderByStarLatingDesc(lectureId);
-        return ApiResponses.success(response, "수강 후기 별점 높은 순 조회입니다.");
-    }
+//    @GetMapping("/review")
+//    @Operation(summary = "수강 후기 전체 목록 조회", description = "Param 설명 :lectureId 꼭 입력해주셔야됩니다. lowness true일 경우 별점 낮은 순 조회입니다. likes true일 경우 좋아요 높은순 조회입니다. recent true일 경우 날짜 최신순 조회입니다. lectureId만 줄 경우 기본값 별점 높은순 조회입니다. ex) ?lectureId=1&lowness=true , ?lectureId=1")
+//    @ApiResponse(responseCode = "200", description = "수강 후기 전체 목록 조회입니다.")
+//    @ApiResponse(responseCode = "202", description = "수강 후기가 어디에도 없습니다.")
+//    public ApiResponses ReviewPostList(@RequestParam("lectureId") Long lectureId,
+//                                       @RequestParam(value = "lowness" , required = false) boolean lowness,
+//                                       @RequestParam(value = "likes", required = false) boolean likes,
+//                                       @RequestParam(value = "recent" , required = false) boolean recent
+//    ){
+//        if(lowness == true){
+//            log.info("별점 낮은 순");
+//            List<ReviewResponse> response = reviewService.findByLectureIdOrderByStarLatingAsc(lectureId);
+//            return ApiResponses.success( response, "수강 후기 별점 낮은 순 조회입니다.");
+//        }else if(likes == true){
+//            log.info("좋아요 높은 순");
+//            List<ReviewResponse> response = reviewService.findByLectureIdOrderByLikesDesc(lectureId);
+//            return ApiResponses.success(response, "수강 후기 좋아요 높은 순 조회입니다.");
+//        }else if(recent == true){
+//            log.info("날짜 최신순");
+//            List<ReviewResponse> response = reviewService.findByLectureIdOrderByCreateDateDesc(lectureId);
+//            return ApiResponses.success(response, "수강 후기 날짜 최신순 조회입니다.");
+//        }
+//
+//        log.info("별점 높은 순");
+//        List<ReviewResponse> response = reviewService.findByLectureIdOrderByStarLatingDesc(lectureId);
+//        return ApiResponses.success(response, "수강 후기 별점 높은 순 조회입니다.");
+//    }
 
     @PostMapping("/review")
     @Operation(summary = "수강 후기 작성 요청", description = "수강 후기 작성을 요청합니다. String postTitle, String postContent, Double starLating, String lecture, Long userNumber 를 json 형태로 body로 전송하시면 됩니다.")
-    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "수강후기 작성이 완료되었습니다.")
-    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "해당 학생이 존재하지 않습니다.")
-    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "40?", description = "해당 수강후기가 존재하지 않습니다.")
-    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "403", description = "수강하지 않은 강의입니다. (reject 처리필요)")
-    public ApiResponse writeReviewPost(@RequestBody ClassReviewRequest request
+    @ApiResponse(responseCode = "200", description = "수강후기 작성이 완료되었습니다.")
+    @ApiResponse(responseCode = "401", description = "해당 학생이 존재하지 않습니다.")
+    @ApiResponse(responseCode = "40?", description = "해당 수강후기가 존재하지 않습니다.")
+    @ApiResponse(responseCode = "403", description = "수강하지 않은 강의입니다. (reject 처리필요)")
+    public Result writeReviewPost(@RequestBody ClassReviewRequest request
     ){
         reviewService.write(request);
-        return ApiResponse.success(null, "수강후기 작성이 완료되었습니다.");
+        return Result.success(null, "수강후기 작성이 완료되었습니다.");
     }
 
     @DeleteMapping("/review")
     @Operation(summary = "수강 후기 삭제 요청", description = "수강 후기 삭제를 요청합니다. Long postId 만 body를 통해서 전송 {} 없이 요청하시면됩니다.")
-    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "수강후기 삭제가 완료되었습니다.")
-    public ApiResponse deleteReviewPost(@RequestBody DeleteReviewRequest request
+    @ApiResponse(responseCode = "200", description = "수강후기 삭제가 완료되었습니다.")
+    public Result deleteReviewPost(@RequestBody DeleteReviewRequest request
     ){
         reviewService.deleteReviewPost(request);
-        return ApiResponse.success(null, "수강후기 삭제가 완료되었습니다.");
+        return Result.success(null, "수강후기 삭제가 완료되었습니다.");
     }
 
     @PatchMapping("/review")
     @Operation(summary = "수강 후기 수정 요청", description = "수강 후기 수정을 요청합니다. Long postId, String postTitle, String postContent, Double starLating, Long important, Long funny, Long difficulty 를 json에 담아서 body로 요청하시면 됩니다.")
-    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "수강후기 수정이 완료되었습니다.")
-    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "403", description = "작성하지 않은 사용자의 요청 입니다.")
-    public ApiResponse updateReviewPost(@RequestBody UpdateReviewRequest request
+    @ApiResponse(responseCode = "200", description = "수강후기 수정이 완료되었습니다.")
+    @ApiResponse(responseCode = "403", description = "작성하지 않은 사용자의 요청 입니다.")
+    public Result updateReviewPost(@RequestBody UpdateReviewRequest request
     ){
         reviewService.updateReviewPost(request); // 수정쪽 예외 한번더 검토해야함
-        return ApiResponse.success(null, "수강후기 수정이 완료되었습니다.");
+        return Result.success(null, "수강후기 수정이 완료되었습니다.");
     }
 
     @PostMapping("/review/like")
     @Operation(summary = "수강 후기에 좋아요 요청", description = "수강 후기 좋아요를 요청합니다.")
-    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "좋아요가 추가되었습니다.")
-    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "202", description = "좋아요가 취소되었습니다.")
-    public ApiResponse likeReviewPost(@Parameter(required = true, description = "int userNumber, Long postId를 json에 담아서 body로 전송") @RequestBody LikeRequest request
+    @ApiResponse(responseCode = "200", description = "좋아요가 추가되었습니다.")
+    @ApiResponse(responseCode = "202", description = "좋아요가 취소되었습니다.")
+    public Result likeReviewPost(@Parameter(required = true, description = "int userNumber, Long postId를 json에 담아서 body로 전송") @RequestBody LikeRequest request
     ){
         String message = reviewService.likeReview(request);
-        return ApiResponse.success(null, message);
+        return Result.success(null, message);
     }
 
     @GetMapping("/review/me")
     @Operation(summary = "해당 학생의 수강후기 전체 조회입니다.", description = "userNumber(학번)을 param을 요청하시면 해당 학생의 수강후기 리스트가 조회됩니다.")
-    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200",description = "해당 학생의 수강후기입니다.")
-    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "202", description = "수강후기가 존재하지 않습니다.")
-    public ApiResponse myReview(@RequestParam("userNumber") int userNumber){
+    @ApiResponse(responseCode = "200",description = "해당 학생의 수강후기입니다.")
+    @ApiResponse(responseCode = "202", description = "수강후기가 존재하지 않습니다.")
+    public Result myReview(@RequestParam("userNumber") int userNumber){
         List<ReviewMeResponse> response = reviewService.findMyReview(userNumber);
-        return ApiResponse.success(response, "해당 학생의 수강후기입니다.");
+        return Result.success(response, "해당 학생의 수강후기입니다.");
     }
 
 }
